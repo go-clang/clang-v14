go-clang/clang-v13

go.mod: init module
Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

go.mod
@@ -1,0 +1,3 @@
+module github.com/go-clang/clang-v13
+
+go 1.17

go-clang/clang-v14

go.mod
@@ -1,0 +1,3 @@
+module github.com/go-clang/clang-v14
+
+go 1.17